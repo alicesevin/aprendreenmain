9d60b5d80540ec18e80c2372e26843b2093e00ae
zelazna <constantin.guidon@gmail.com>
2017-01-26 09:56:04 +0100

    gitignore

--- wp-content/themes/aprendreenmain/app/js/main.js
+++ wp-content/themes/aprendreenmain/app/js/main.js
@@ -1,18 +1,11 @@
 // Require des composants
 var appGlobal = require('./components/global');
-<<<<<<< HEAD
 var animationParallax = require('./components/animationParallax');
-=======
 var startAnimation = require('./components/startAnime');
-<<<<<<< HEAD
->>>>>>> start anime
-// Initialisation des globaux
-appGlobal();
-animationParallax();
-=======
 var scrollAnimation = require('./components/scrollAnimation');
+
 // Initialisation des globaux
 appGlobal();
 startAnimation();
 scrollAnimation();
->>>>>>> scrollMagic
+animationParallax();
